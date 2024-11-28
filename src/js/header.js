47b0cfed4cacdd2492ebec14770f2257888a0835
José Murilo <82318481+jmurilojm@users.noreply.github.com>
2024-11-28 19:48:10 -0300

    Update header.js

--- src/js/header.js
+++ src/js/header.js
@@ -1,11 +1,12 @@
-const horario = '19:33';
+const horario = '19:48';
 const data = '28/11/2024';
 const logo = 'src/imagens/logo.png';
+const editalAtualizado = 'src/midia/EDITAL - GUARDA DE ARCOVERDE - ATUALIZADO EM 28.11.2024.pdf'
 
 
 const cabecalho = `<div class="importante">
       <p><em>Página não Oficial!</em></p>
-      <p>Última atualização: <span>${horario}</span> horas - <span>${data}</span></p>
+      <p>Ultima atualização: <span>${horario}</span> horas - <span>${data}</span></p>
     </div>
 
     <img src="${logo}" alt="Logo não oficial da GCM Arcoverde/PE. Arte: José Murilo.">
@@ -13,6 +14,10 @@ const cabecalho = `<div class="importante">
     <h2>APROVADOS/2024</h2>
     <h6><em>*Fases, pós Prova Objetiva, ainda em andamento.</em></h6>`;
 
+const arquivos = `<strong><p>Arquivos para Download</p></strong>
+<a href="${editalAtualizado}" download=""><button>Edital Atualizado - 28/11/2024</button></a>`
+
 
 
 document.querySelector('header').innerHTML = cabecalho;
+document.querySelector('section').innerHTML = arquivos;

873ce181425d020826588e1b694eafaeeb37877b
José Murilo <82318481+jmurilojm@users.noreply.github.com>
2024-11-28 23:24:33 -0300

    Update listaPCD.js

--- src/js/listaPCD.js
+++ src/js/listaPCD.js
@@ -5,158 +5,158 @@ const candidatosPCD = [
     "nome": "ROBERTO ANTONIO DOS SANTOS SILVA",
     "taf": true,
     "psi": true,
-    "em": true,
-    "fic": true
+    "fic": true,
+    "em": true
   },
   {
     "matricula": "0234943",
     "nome": "YBN ALCANTARA FERRO DE MOURA",
     "taf": false,
     "psi": false,
-    "em": "aguardando",
-    "fic": "aguardando"
+    "fic": "aguardando",
+    "em": "aguardando"
   },
   {
     "matricula": "0224011",
     "nome": "FRANCIMARIO GONÇALVES DA SILVA",
     "taf": "aguardando",
     "psi": true,
-    "em": "aguardando",
-    "fic": "aguardando"
+    "fic": "aguardando",
+    "em": "aguardando"
   },
   {
     "matricula": "0217404",
     "nome": "IAGO DE FARIAS ARAUJO",
     "taf": true,
     "psi": true,
-    "em": "aguardando",
-    "fic": "aguardando"
+    "fic": "aguardando",
+    "em": "aguardando"
   },
   {
     "matricula": "0222152",
     "nome": "PABLO MASCENA ALENCAR",
     "taf": true,
     "psi": false,
-    "em": true,
-    "fic": true
+    "fic": true,
+    "em": true
   },
   {
     "matricula": "0215226",
     "nome": "RALF WANDER LEITE DE LIMA",
     "taf": false,
     "psi": false,
-    "em": "aguardando",
-    "fic": "aguardando"
+    "fic": "aguardando",
+    "em": "aguardando"
   },
   {
     "matricula": "0231949",
     "nome": "JOSÉ LADJANE MENDES E SILVA",
     "taf": true,
     "psi": "aguardando",
-    "em": "aguardando",
-    "fic": "aguardando"
+    "fic": "aguardando",
+    "em": "aguardando"
   },
   {
     "matricula": "0223935",
     "nome": "PAULO RICARDO VALOES MIRANDA",
     "taf": true,
     "psi": false,
-    "em": "aguardando",
-    "fic": "aguardando"
+    "fic": "aguardando",
+    "em": "aguardando"
   },
   {
     "matricula": "0235803",
     "nome": "ISRAEL MARTINS SOUZA GOMES",
     "taf": "aguardando",
     "psi": "aguardando",
-    "em": "aguardando",
-    "fic": "aguardando"
+    "fic": "aguardando",
+    "em": "aguardando"
   },
   {
     "matricula": "0228563",
     "nome": "ROMERIO ARAUJO GOMES",
     "taf": "aguardando",
     "psi": "aguardando",
-    "em": "aguardando",
-    "fic": "aguardando"
+    "fic": "aguardando",
+    "em": "aguardando"
   },
   {
     "matricula": "0226175",
     "nome": "JOSENILDO BEZERRA DA SILVA",
     "taf": "aguardando",
     "psi": "aguardando",
-    "em": "aguardando",
-    "fic": "aguardando"
+    "fic": "aguardando",
+    "em": "aguardando"
   },
   {
     "matricula": "0214575",
     "nome": "ALESSANDRO LESSA RODRIGUES",
     "taf": true,
     "psi": true,
-    "em": true,
-    "fic": true
+    "fic": true,
+    "em": true
   },
   {
     "matricula": "0230134",
     "nome": "WILLIANS BEZERRA LEITE",
     "taf": "aguardando",
     "psi": "aguardando",
-    "em": "aguardando",
-    "fic": "aguardando"
+    "fic": "aguardando",
+    "em": "aguardando"
   },
   {
     "matricula": "0236630",
     "nome": "JOSE HENRIQUE INACIO DANTAS",
     "taf": true,
     "psi": true,
-    "em": true,
-    "fic": true
+    "fic": true,
+    "em": true
   },
   {
     "matricula": "0235710",
     "nome": "RONALDO JOSÉ DE BRITO",
     "taf": "aguardando",
     "psi": "aguardando",
-    "em": "aguardando",
-    "fic": "aguardando"
+    "fic": "aguardando",
+    "em": "aguardando"
   },
   {
     "matricula": "0220555",
     "nome": "JEANDERSON DA SILVA CARVALHO",
     "taf": true,
     "psi": true,
-    "em": "aguardando",
-    "fic": "aguardando"
+    "fic": "aguardando",
+    "em": "aguardando"
   },
   {
     "matricula": "0213453",
     "nome": "KATIA MARIA FERREIRA PIMENTEL",
     "taf": "aguardando",
     "psi": "aguardando",
-    "em": "aguardando",
-    "fic": "aguardando"
+    "fic": "aguardando",
+    "em": "aguardando"
   },
   {
     "matricula": "0222611",
     "nome": "WAGNER HENRIQUE PEREIRA CRISOSTOMO",
     "taf": "aguardando",
     "psi": "aguardando",
-    "em": "aguardando",
-    "fic": "aguardando"
+    "fic": "aguardando",
+    "em": "aguardando"
   },
   {
     "matricula": "0219096",
     "nome": "JEFERSON GOMES FERREIRA",
     "taf": "aguardando",
     "psi": "aguardando",
-    "em": "aguardando",
-    "fic": "aguardando"
+    "fic": "aguardando",
+    "em": "aguardando"
   },
   {
     "matricula": "0233183",
     "nome": "JANIEL DE ANDRADE BEZERRA",
     "taf": true,
     "psi": true,
-    "em": "aguardando",
-    "fic": "aguardando"
+    "fic": "aguardando",
+    "em": "aguardando"
   }];

194ec3e9ff04813a16167b113ee23aebcf2b71a6
José Murilo <j.murilo.87@gmail.com>
2024-12-06 15:06:21 -0300

    update listaFeminino.js ‐ organizando a lista.

--- src/js/listaFeminino.js
+++ src/js/listaFeminino.js
@@ -1,25 +1,185 @@
 // lista feminino
 const candidatosFeminino = [
-  { "matricula": "213423", "nome": "GABRIELLA TENÓRIO GUIMARÃES", "taf": true, "psi": true, "fic": true, "em": true, "id": 1 },
-  { "matricula": "225496", "nome": "JOYCE FREIRE BARBOSA DA SILVA", "taf": true, "psi": true, "fic": true, "em": true, "id": 2 },
-  { "matricula": "213435", "nome": "BEATRIZ MAURÍCIO TORRES MACEDO", "taf": true, "psi": true, "fic": false, "em": true, "id": 3 },
-  { "matricula": "224398", "nome": "JAQUELINE PATRICIA RICARDO HONORIO", "taf": true, "psi": true, "fic": true, "em": true, "id": 4 },
-  { "matricula": "226200", "nome": "MARIA TEREZA FEITOSA TELES", "taf": false, "psi": false, "fic": "aguardando", "em": "aguardando", "id": 5 },
-  { "matricula": "217046", "nome": "ANA KAROLINE FRAGA NUNES", "taf": true, "psi": true, "fic": true, "em": true, "id": 6 },
-  { "matricula": "221221", "nome": "EDILAINE ARAUJO SILVA", "taf": false, "psi": false, "fic": "aguardando", "em": "aguardando", "id": 7 },
-  { "matricula": "235627", "nome": "MARIA CÂNDIDA SOARES SILVA", "taf": false, "psi": false, "fic": "aguardando", "em": "aguardando", "id": 8 },
-  { "matricula": "213677", "nome": "ANDRÉIA CAVALCANTI GOMES", "taf": true, "psi": true, "fic": true, "em": true, "id": 9 },
-  { "matricula": "235461", "nome": "YNGRID CAMILE SILVA PEREIRA", "taf": true, "psi": true, "fic": true, "em": true, "id": 10 },
-  { "matricula": "235672", "nome": "SAMYRYS ALVES NERES", "taf": false, "psi": false, "fic": "aguardando", "em": "aguardando", "id": 11 },
-  { "matricula": "217957", "nome": "JÉSSIKA KAROLLYNA DE SOUZA SILVA", "taf": false, "psi": false, "fic": "aguardando", "em": "aguardando", "id": 12 },
-  { "matricula": "220468", "nome": "JESSICA MAITANA SANTOS CAVALCANTI", "taf": true, "psi": true, "fic": true, "em": true, "id": 13 },
-  { "matricula": "213942", "nome": "JESSICA ELIZANDRA DE LIMA", "taf": true, "psi": true, "fic": true, "em": true, "id": 14 },
-  { "matricula": "231736", "nome": "JOSELAINE MARIA DOS SANTOS SILVA", "taf": "aguardando", "psi": "aguardando", "fic": "aguardando", "em": "aguardando", "id": 15 },
-  { "matricula": "235233", "nome": "CARLYZE CRISTYANE DE OLIVEIRA SILVA", "taf": true, "psi": true, "fic": true, "em": false, "id": 16 },
-  { "matricula": "233331", "nome": "KATHARINA MACIEL MARINHO SOARES", "taf": true, "psi": true, "fic": true, "em": true, "id": 17 },
-  { "matricula": "231198", "nome": "GESSICA LAYANNY DE MORAIS NUNES", "taf": true, "psi": false, "fic": "aguardando", "em": "aguardando", "id": 18 },
-  { "matricula": "235292", "nome": "NATALIA PEREIRA DA SILVA SOUZA", "taf": true, "psi": true, "fic": true, "em": true, "id": 19 },
-  { "matricula": "226853", "nome": "RUANE LARISSA DOS SANTOS MORAES", "taf": false, "psi": false, "fic": "aguardando", "em": "aguardando", "id": 20 }];
+  { 
+    "matricula": "213423", 
+    "nome": "GABRIELLA TENÓRIO GUIMARÃES", 
+    "taf": true, 
+    "psi": true, 
+    "fic": true, 
+    "em": true, 
+    "id": 1 
+  },
+  { 
+    "matricula": "225496", 
+    "nome": "JOYCE FREIRE BARBOSA DA SILVA", 
+    "taf": true, 
+    "psi": true, 
+    "fic": true, 
+    "em": true, 
+    "id": 2 
+  },
+  { 
+    "matricula": "213435", 
+    "nome": "BEATRIZ MAURÍCIO TORRES MACEDO", 
+    "taf": true, 
+    "psi": true, 
+    "fic": false, 
+    "em": true, 
+    "id": 3 
+  },
+  { 
+    "matricula": "224398", 
+    "nome": "JAQUELINE PATRICIA RICARDO HONORIO", 
+    "taf": true, 
+    "psi": true, 
+    "fic": true, 
+    "em": true, 
+    "id": 4 
+  },
+  { 
+    "matricula": "226200", 
+    "nome": "MARIA TEREZA FEITOSA TELES", 
+    "taf": false, 
+    "psi": false, 
+    "fic": "aguardando", 
+    "em": "aguardando", 
+    "id": 5 
+  },
+  { 
+    "matricula": "217046", 
+    "nome": "ANA KAROLINE FRAGA NUNES", 
+    "taf": true, 
+    "psi": true, 
+    "fic": true, 
+    "em": true, 
+    "id": 6 
+  },
+  { 
+    "matricula": "221221", 
+    "nome": "EDILAINE ARAUJO SILVA", 
+    "taf": false, 
+    "psi": false, 
+    "fic": "aguardando", 
+    "em": "aguardando", 
+    "id": 7 
+  },
+  { 
+    "matricula": "235627", 
+    "nome": "MARIA CÂNDIDA SOARES SILVA", 
+    "taf": false, 
+    "psi": false, 
+    "fic": "aguardando", 
+    "em": "aguardando", 
+    "id": 8 
+  },
+  { 
+    "matricula": "213677", 
+    "nome": "ANDRÉIA CAVALCANTI GOMES", 
+    "taf": true, 
+    "psi": true, 
+    "fic": true, 
+    "em": true, 
+    "id": 9 
+  },
+  { 
+    "matricula": "235461", 
+    "nome": "YNGRID CAMILE SILVA PEREIRA", 
+    "taf": true, 
+    "psi": true, 
+    "fic": true, 
+    "em": true, 
+    "id": 10 
+  },
+  { 
+    "matricula": "235672", 
+    "nome": "SAMYRYS ALVES NERES", 
+    "taf": false, 
+    "psi": false, 
+    "fic": "aguardando", 
+    "em": "aguardando", 
+    "id": 11 
+  },
+  { 
+    "matricula": "217957", 
+    "nome": "JÉSSIKA KAROLLYNA DE SOUZA SILVA", 
+    "taf": false, 
+    "psi": false, 
+    "fic": "aguardando", 
+    "em": "aguardando", 
+    "id": 12 
+  },
+  { 
+    "matricula": "220468", 
+    "nome": "JESSICA MAITANA SANTOS CAVALCANTI", 
+    "taf": true, 
+    "psi": true, 
+    "fic": true, 
+    "em": true, 
+    "id": 13 
+  },
+  { 
+    "matricula": "213942", 
+    "nome": "JESSICA ELIZANDRA DE LIMA", 
+    "taf": true, 
+    "psi": true, 
+    "fic": true, 
+    "em": true, 
+    "id": 14 
+  },
+  { 
+    "matricula": "231736", 
+    "nome": "JOSELAINE MARIA DOS SANTOS SILVA", 
+    "taf": "aguardando", 
+    "psi": "aguardando", 
+    "fic": "aguardando", 
+    "em": "aguardando", 
+    "id": 15 
+  },
+  { 
+    "matricula": "235233", 
+    "nome": "CARLYZE CRISTYANE DE OLIVEIRA SILVA", 
+    "taf": true, 
+    "psi": true, 
+    "fic": true, 
+    "em": false, 
+    "id": 16 
+  },
+  { 
+    "matricula": "233331", 
+    "nome": "KATHARINA MACIEL MARINHO SOARES", 
+    "taf": true, 
+    "psi": true, 
+    "fic": true, 
+    "em": true, 
+    "id": 17 
+  },
+  { 
+    "matricula": "231198", 
+    "nome": "GESSICA LAYANNY DE MORAIS NUNES", 
+    "taf": true, 
+    "psi": false, 
+    "fic": "aguardando", 
+    "em": "aguardando", 
+    "id": 18 
+  },
+  { 
+    "matricula": "235292", 
+    "nome": "NATALIA PEREIRA DA SILVA SOUZA", 
+    "taf": true, 
+    "psi": true, 
+    "fic": true, 
+    "em": true, 
+    "id": 19 
+  },
+  { 
+    "matricula": "226853", 
+    "nome": "RUANE LARISSA DOS SANTOS MORAES", 
+    "taf": false, 
+    "psi": false, 
+    "fic": "aguardando", 
+    "em": "aguardando", 
+    "id": 20 
+  }];
   
   
   

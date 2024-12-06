2190afa90fb47c82c7a75ca255222264a2623ee9
José Murilo <j.murilo.87@gmail.com>
2024-12-06 12:29:47 -0300

    update listaPCD.js ‐ organizando a estrutura da lista.

--- src/js/listaPCD.js
+++ src/js/listaPCD.js
@@ -1,25 +1,184 @@
 // lista pcd
 const candidatosPCD = [
-  { "matricula": "0230648", "nome": "ROBERTO ANTONIO DOS SANTOS SILVA", "taf": true, "psi": true, "fic": true, "em": true, "id": 1 },
-  { "matricula": "0234943", "nome": "YBN ALCANTARA FERRO DE MOURA", "taf": false, "psi": false, "fic": "aguardando", "em": "aguardando", "id": 2 },
-  { "matricula": "0224011", "nome": "FRANCIMARIO GONÇALVES DA SILVA", "taf": true, "psi": true, "fic": false, "em": true, "id": 3 },
-  { "matricula": "0217404", "nome": "IAGO DE FARIAS ARAUJO", "taf": true, "psi": true, "fic": "aguardando", "em": "aguardando", "id": 4 },
-  { "matricula": "0222152", "nome": "PABLO MASCENA ALENCAR", "taf": true, "psi": false, "fic": true, "em": true, "id": 5 },
-  { "matricula": "0215226", "nome": "RALF WANDER LEITE DE LIMA", "taf": false, "psi": false, "fic": "aguardando", "em": "aguardando", "id": 6 },
-  { "matricula": "0231949", "nome": "JOSÉ LADJANE MENDES E SILVA", "taf": true, "psi": true, "fic": false, "em": true, "id": 7 },
-  { "matricula": "0223935", "nome": "PAULO RICARDO VALOES MIRANDA", "taf": true, "psi": false, "fic": "aguardando", "em": "aguardando", "id": 8 },
-  { "matricula": "0235803", "nome": "ISRAEL MARTINS SOUZA GOMES", "taf": true, "psi": true, "fic": true, "em": false, "id": 9 },
-  { "matricula": "0228563", "nome": "ROMERIO ARAUJO GOMES", "taf": "aguardando", "psi": "aguardando", "fic": "aguardando", "em": "aguardando", "id": 10 },
-  { "matricula": "0226175", "nome": "JOSENILDO BEZERRA DA SILVA", "taf": "aguardando", "psi": "aguardando", "fic": "aguardando", "em": "aguardando", "id": 11 },
-  { "matricula": "0214575", "nome": "ALESSANDRO LESSA RODRIGUES", "taf": true, "psi": true, "fic": true, "em": true, "id": 12 },
-  { "matricula": "0230134", "nome": "WILLIANS BEZERRA LEITE", "taf": "aguardando", "psi": "aguardando", "fic": "aguardando", "em": "aguardando", "id": 13 },
-  { "matricula": "0236630", "nome": "JOSE HENRIQUE INACIO DANTAS", "taf": true, "psi": true, "fic": true, "em": true, "id": 14 },
-  { "matricula": "0235710", "nome": "RONALDO JOSÉ DE BRITO", "taf": "aguardando", "psi": "aguardando", "fic": "aguardando", "em": "aguardando", "id": 15 },
-  { "matricula": "0220555", "nome": "JEANDERSON DA SILVA CARVALHO", "taf": true, "psi": true, "fic": true, "em": true, "id": 16 },
-  { "matricula": "0213453", "nome": "KATIA MARIA FERREIRA PIMENTEL", "taf": "aguardando", "psi": "aguardando", "fic": "aguardando", "em": "aguardando", "id": 17 },
-  { "matricula": "0222611", "nome": "WAGNER HENRIQUE PEREIRA CRISOSTOMO", "taf": "aguardando", "psi": "aguardando", "fic": "aguardando", "em": "aguardando", "id": 18 },
-  { "matricula": "0219096", "nome": "JEFERSON GOMES FERREIRA", "taf": "aguardando", "psi": "aguardando", "fic": "aguardando", "em": "aguardando", "id": 19 },
-  { "matricula": "0233183", "nome": "JANIEL DE ANDRADE BEZERRA", "taf": true, "psi": true, "fic": "aguardando", "em": "aguardando", "id": 20 }];
+  { 
+    "matricula": "0230648", 
+    "nome": "ROBERTO ANTONIO DOS SANTOS SILVA", 
+    "taf": true, 
+    "psi": true, 
+    "fic": true, 
+    "em": true, 
+    "id": 1 
+  },
+  { 
+    "matricula": "0234943", 
+    "nome": "YBN ALCANTARA FERRO DE MOURA", 
+    "taf": false, 
+    "psi": false, 
+    "fic": "aguardando", 
+    "em": "aguardando", "id": 2 
+  },
+  { 
+    "matricula": "0224011", 
+    "nome": "FRANCIMARIO GONÇALVES DA SILVA", 
+    "taf": true, 
+    "psi": true, 
+    "fic": false, 
+    "em": true, 
+    "id": 3 
+  },
+  { 
+    "matricula": "0217404", 
+    "nome": "IAGO DE FARIAS ARAUJO", 
+    "taf": true, 
+    "psi": true, 
+    "fic": "aguardando", 
+    "em": "aguardando", 
+    "id": 4 
+  },
+  { 
+    "matricula": "0222152", 
+    "nome": "PABLO MASCENA ALENCAR", 
+    "taf": true, 
+    "psi": false, 
+    "fic": true, 
+    "em": true, 
+    "id": 5 
+  },
+  { 
+    "matricula": "0215226", 
+    "nome": "RALF WANDER LEITE DE LIMA", 
+    "taf": false, 
+    "psi": false, 
+    "fic": "aguardando", 
+    "em": "aguardando", 
+    "id": 6 
+  },
+  { 
+    "matricula": "0231949", 
+    "nome": "JOSÉ LADJANE MENDES E SILVA", 
+    "taf": true, 
+    "psi": true, 
+    "fic": false, 
+    "em": true, 
+    "id": 7 
+  },
+  { 
+    "matricula": "0223935", 
+    "nome": "PAULO RICARDO VALOES MIRANDA", 
+    "taf": true, 
+    "psi": false, 
+    "fic": "aguardando", 
+    "em": "aguardando", 
+    "id": 8 
+  },
+  { 
+    "matricula": "0235803", 
+    "nome": "ISRAEL MARTINS SOUZA GOMES", 
+    "taf": true, 
+    "psi": true, 
+    "fic": true, 
+    "em": false, 
+    "id": 9 
+  },
+  { 
+    "matricula": "0228563", 
+    "nome": "ROMERIO ARAUJO GOMES", 
+    "taf": "aguardando", 
+    "psi": "aguardando", 
+    "fic": "aguardando", 
+    "em": "aguardando", 
+    "id": 10 
+  },
+  { 
+    "matricula": "0226175", 
+    "nome": "JOSENILDO BEZERRA DA SILVA", 
+    "taf": "aguardando", 
+    "psi": "aguardando", 
+    "fic": "aguardando", 
+    "em": "aguardando", 
+    "id": 11 
+  },
+  { 
+    "matricula": "0214575", 
+    "nome": "ALESSANDRO LESSA RODRIGUES", 
+    "taf": true, 
+    "psi": true, 
+    "fic": true, 
+    "em": true, 
+    "id": 12 
+  },
+  { 
+    "matricula": "0230134", 
+    "nome": "WILLIANS BEZERRA LEITE", 
+    "taf": "aguardando", 
+    "psi": "aguardando", 
+    "fic": "aguardando", 
+    "em": "aguardando", 
+    "id": 13 
+  },
+  { 
+    "matricula": "0236630", 
+    "nome": "JOSE HENRIQUE INACIO DANTAS", 
+    "taf": true, 
+    "psi": true, 
+    "fic": true, 
+    "em": true, 
+    "id": 14 
+  },
+  { 
+    "matricula": "0235710", 
+    "nome": "RONALDO JOSÉ DE BRITO", 
+    "taf": "aguardando", 
+    "psi": "aguardando", 
+    "fic": "aguardando", 
+    "em": "aguardando", 
+    "id": 15 
+  },
+  { 
+    "matricula": "0220555", 
+    "nome": "JEANDERSON DA SILVA CARVALHO", 
+    "taf": true, 
+    "psi": true, 
+    "fic": true, 
+    "em": true, 
+    "id": 16 
+  },
+  { 
+    "matricula": "0213453", 
+    "nome": "KATIA MARIA FERREIRA PIMENTEL", 
+    "taf": "aguardando", 
+    "psi": "aguardando", 
+    "fic": "aguardando", 
+    "em": "aguardando", 
+    "id": 17 
+  },
+  { 
+    "matricula": "0222611", 
+    "nome": "WAGNER HENRIQUE PEREIRA CRISOSTOMO", 
+    "taf": "aguardando", 
+    "psi": "aguardando", 
+    "fic": "aguardando", 
+    "em": "aguardando", 
+    "id": 18 
+  },
+  { 
+    "matricula": "0219096", 
+    "nome": "JEFERSON GOMES FERREIRA", 
+    "taf": "aguardando", 
+    "psi": "aguardando", 
+    "fic": "aguardando", 
+    "em": "aguardando", 
+    "id": 19 
+  },
+  { 
+    "matricula": "0233183", 
+    "nome": "JANIEL DE ANDRADE BEZERRA", 
+    "taf": true, 
+    "psi": true, 
+    "fic": "aguardando", 
+    "em": "aguardando", 
+    "id": 20 
+  }];
   
   
   
